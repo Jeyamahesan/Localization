e0c1f0946e8a4e2c6c2257a1f8a38aadd0dc4728
Jeyamahesan Chandrakanthan <jeyamahesan@kappsoft.com>
2016-08-29 11:53:59 +0530

    Initial Commit

---
+++ LocalizationDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LocalizationDemo
+//
+//  Created by Jey on 8/29/16.
+//  Copyright © 2016 Jey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LocalizationDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LocalizationDemo
+//
+//  Created by Jey on 8/29/16.
+//  Copyright © 2016 Jey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
